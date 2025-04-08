2155f899c498b98c541fea79ecf5ba2f68bf9190
Alvaro Valero <alvaro.valero.garcia.al@iespoligonosur.org>
2025-04-08 18:52:07 +0200

    Creación proyecto

---
+++ PixelGround/src/main/java/com/PixelGround/back/PixelGroundApplication.java
@@ -0,0 +1,13 @@
+package com.PixelGround.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelGroundApplication.class, args);
+	}
+
+}
---
+++ PixelGround/src/test/java/com/PixelGround/back/PixelGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PixelGround.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
